7ac9ee17742ba972859b9100e8dafdd830da76ab
GabrielBL <gabriel.lamezon@outlook.com>
2025-09-02 15:01:28 -0300

    2nd changes made in 2025-09-02

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import LoginPage from "./pages/Login.jsx";
+import LoginPage from "./components/Login.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
